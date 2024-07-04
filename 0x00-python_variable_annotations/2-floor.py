e4b451832b62a3b471f8e2c8b15a5df6ad59746d
Michael Iyke <ymichaelc@gmail.com>
2024-07-04 17:33:47 +0100

    Fixed task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Python Variable Annotations module"""
+import math
 
 
-def floor(a: float, b: float) -> int:
+def floor(a: float) -> int:
     """Return the floor of two floating point numbers
     Floor is here simply means the integer division of a and b"""
-    return a // b
+    # return floor of a
+    return math.floor(a)
