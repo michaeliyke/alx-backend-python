20820743dfceee070d4b838addfe713bb498d07d
Michael Iyke <ymichaelc@gmail.com>
2024-07-04 18:11:13 +0100

    Fixed task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Python Variable Annotations module"""
-from typing import List, Tuple, Sequence
+from typing import Iterable, Tuple, Sequence, List
 
 
-def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of the length of each element in the input list"""
     return [(i, len(i)) for i in lst]
