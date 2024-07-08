18d06ab0e5d7d9ef422a2448c9a5a9a503f909a3
Michael Iyke <ymichaelc@gmail.com>
2024-07-08 21:32:32 +0100

    Task 1: Spaw wait_random n times, return result ascending as completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,3 +2,12 @@
 """Module on Async/await lessons"""
 
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+    Spawn wait_random n times with max_delay. Return the list of all delays.
+    """
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
