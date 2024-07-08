360926a4ccb07e7affdfc043513101e5c842ae03
Michael Iyke <ymichaelc@gmail.com>
2024-07-08 22:57:40 +0100

    Task 04: re-write wait_n to use task_wait_random

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,3 +2,12 @@
 """Module on Async/await lessons"""
 
 import asyncio
+from typing import List
+from asyncio import Future
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Return the list of all the delays"""
+    delays: List[Future] = [task_wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
