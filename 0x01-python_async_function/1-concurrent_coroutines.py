d67383ed3550baf729641db4132e927ee37e74be
Michael Iyke <ymichaelc@gmail.com>
2024-07-08 22:36:36 +0100

    Correct 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,12 +2,13 @@
 """Module on Async/await lessons"""
 
 import asyncio
+from typing import List
+from asyncio import Future
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times with max_delay. Return the list of all delays.
     """
-    Spawn wait_random n times with max_delay. Return the list of all delays.
-    """
-    delays = [wait_random(max_delay) for _ in range(n)]
+    delays: List[Future[float]] = [wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in asyncio.as_completed(delays)]
