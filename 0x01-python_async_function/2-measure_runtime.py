b81282cf4e3e8e660e2c5aef3e9f19eeab49d25d
Michael Iyke <ymichaelc@gmail.com>
2024-07-08 22:42:44 +0100

    Task 02: measure execution time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,3 +2,13 @@
 """Module on Async/await lessons"""
 
 import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the execution time of wait_n coroutine"""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    return (end - start) / n
