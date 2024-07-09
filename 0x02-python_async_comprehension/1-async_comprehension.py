73cdd815be4fa4389092384e0d7dec53ea9c0a37
Michael Iyke <ymichaelc@gmail.com>
2024-07-10 00:08:42 +0100

    Task 02

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,10 @@
 """Module: Async Comprehension lessons"""
 
 from typing import List, AsyncGenerator
-async_generator = __import__('0-async_generator').async_generator
+async_generator: AsyncGenerator[float, None] =\
+    __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """Loop through async generator and return list of results"""
-    asyncGen: AsyncGenerator[float, None] = async_generator()
-    return [i async for i in asyncGen]
+    return [i async for i in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env async_comprehension
 """Module: Async Comprehension lessons"""
 
 import asyncio
+import time
+from typing import Coroutine, List
+async_comprehension: Coroutine[None, None, List[float]] =\
+    __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """Measure runtime of async_comprehension coroutine"""
+    start = time.time()
+    await async_comprehension()
+    end = time.time()
+    return end - start
