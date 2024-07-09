08740417f4be760fb39eeb2cc0ba189fa5e9e80d
Michael Iyke <ymichaelc@gmail.com>
2024-07-09 13:24:43 +0100

    Task 00: Loop 10x, wait a sec asyc, yield random no

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,3 +2,12 @@
 """Module: Async Comprehension lessons"""
 
 import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Loops 10 times, wait a sec, yields a random number between 0 and 10."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
