9094c254a1715d4fb58c7effbd45718016b8e0dc
Michael Iyke <ymichaelc@gmail.com>
2024-07-29 13:49:05 +0100

    Unittesting: right return value

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""Tests for github org utils."""
+import json
+from unittest.mock import patch, Mock
+import unittest
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test access_nested_map."""
+
+    def test_access_nested_map(self):
+        """Test access_nested_map returns the right value."""
+        nested_map = {"a": {"b": {"c": 1}}}
+        self.assertEqual(access_nested_map(nested_map, ["a", "b", "c"]), 1)
