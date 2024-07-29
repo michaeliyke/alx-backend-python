e2713c0884721fe9b97bc58e801138614ce70436
Michael Iyke <ymichaelc@gmail.com>
2024-07-29 14:35:06 +0100

    Correction: test properly with parameterized.expand

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 import json
 from unittest.mock import patch, Mock
 import unittest
+from parameterized import parameterized
 
 from utils import (
     access_nested_map,
@@ -12,8 +13,14 @@ from utils import (
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test access_nested_map."""
+    """Testing access_nested_map."""
 
-    def test_access_nested_map(self):
-        """Test access_nested_map."""
-        assert access_nested_map({"a": {"b": {"c": 1}}}, ["a", "b", "c"]) == 1
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": {"c": 1}}}, ("a", "b", "c"), 1),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map returns the right value."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
